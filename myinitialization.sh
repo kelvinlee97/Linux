2b5a70904cc1758dce3a502f5a4bb24f37483a93
KelvinLee <44488127+White97k@users.noreply.github.com>
2021-07-04 21:03:28 +0800

    update env settings

--- myinitialization.sh
+++ myinitialization.sh
@@ -14,3 +14,15 @@ systemctl disable firewalld
 
 #download apache tomcat 8
 wget https://downloads.apache.org/tomcat/tomcat-8/v8.5.68/bin/apache-tomcat-8.5.68.tar.gz /opt
+
+
+
+#set enviroment variable for JAVA 8 ( NOT COMPLETE )
+JAVA_HOME=/opt/jdk
+export JAVA_HOME
+CLASSPATH=.:$JAVA_HOME/lirootb:$JAVA_HOME/jre/lib
+export CLASSPATH
+JRE=$JAVA_HOME/jre
+export JRE
+PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
+export PATH

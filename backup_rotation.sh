5f5c212d27eef201bf45e2cddbad4a8e05dfbeb1
KelvinLee <xiiaob799@gmail.com>
2024-11-11 14:38:53 +0800

    modified:   backup_rotation.sh

--- backup_rotation.sh
+++ backup_rotation.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-backup_dir="/path/to/backups"
-max_backups=5
+backup_dir="/path/to/backups" # edit
+max_backups=5 # edit
 
 while [ $(ls -1 "$backup_dir" | wc -l) -gt "$max_backups" ]; do
     oldest_backup=$(ls -1t "$backup_dir" | tail -n 1)
+    # this action will keep delete the latest file until meet the condition "max_backups"
     rm -r "$backup_dir/$oldest_backup"
 done
 
